9497a882942c9415fd82fca667a33ad3809c011b
Ahafid <ahafidkabir11@gmail.com>
2024-03-24 12:36:32 +0600

    Add files via upload

---
+++ ExecutionSteps/src/executionsteps/ExecutionSteps.java
@@ -0,0 +1,17 @@
+
+package executionsteps;
+
+public class ExecutionSteps {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");//javva execute in 5 steps 
+        /*edid, compile ,load, verify , execute
+        // text-> .java (file save with this extension) -> 
+        (run/compile the .java code ) -> .class (crete byte code .class file which is not human readable)
+        ->(byte code java is secure )-> jvm run the .class file 
+        ->create executable file,
+        edit(.java) -> compile (.class)-> load to memory ->verify -> execution
+        */
+    }
+    
+}
